dfebb282075e09013f7921921ed5fc606d70b78e
Max <linme00p@gmail.com>
2021-07-06 15:58:13 +0300

    Add container for Card.svelte in App component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,19 @@
     import Card from './Card.svelte'
 </script>
 
-<div>
-    <Card />
+<style>
+    .container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    .container > div {
+        width: calc(100% / 3);
+    }
+</style>
+
+<div class="container">
+    <div>
+        <Card />
+    </div>
 </div>

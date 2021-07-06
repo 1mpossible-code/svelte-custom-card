c50d874f43b9aa4a48b03ba555d15f5a81e10a61
Max <linme00p@gmail.com>
2021-07-06 15:55:05 +0300

    Add base structure of Card.svelte

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,10 +1,19 @@
-<div>
-    <h1>Hello {name}!</h1>
+<div class="container">
+    <img src="https://via.placeholder.com/320x180/" alt="320x180">
+    <div>
+        <h3>Title</h3>
+    </div>
+    <div class="description">
+        <p>Description</p>
+    </div>
 </div>
 
 <style>
-    h1 {
-        font-size: 16px;
+    .container {
+        border: 1px solid silver;
+        width: 100%;
+        border-radius: 25px;
+        overflow: hidden;
     }
 </style>
 

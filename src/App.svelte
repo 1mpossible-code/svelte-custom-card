531e94cb31d936d1eaf0f79d836f080ad1262aee
Max <linme00p@gmail.com>
2021-07-06 16:31:16 +0300

    Update App.svelte. Create second Card with the almost same content. Use different way to display link in Card component

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,22 @@
         justify-content: center;
     }
     .container > div {
-        width: calc(100% / 3);
+        width: calc(97% / 3);
+        margin: 0 1%;
+    }
+    a {
+        padding: 7px 20px;
+        text-decoration: none;
+        border: 1px solid silver;
+        border-radius: 10px;
+        color: white;
+        background-color: darkcyan;
+        transition: .3s;
+    }
+
+    a:hover {
+        filter: brightness(110%);
+        transform: scale(1.1);
     }
 </style>
 
@@ -18,6 +33,13 @@
         <Card image="https://via.placeholder.com/320x180/" link="https://google.com" linkTitle="Link">
             <h3 slot="title">Title</h3>
             <p slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ratione!</p>
+        </Card>
+    </div>
+    <div>
+        <Card image="https://via.placeholder.com/320x180/">
+            <h3 slot="title">Title 2</h3>
+            <p slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ratione!</p>
+            <a slot="link" href="https://google.com">Link</a>
         </Card>
     </div>
 </div>
